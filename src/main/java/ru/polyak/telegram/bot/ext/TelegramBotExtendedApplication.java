38d17fade9573e06d5969d3636c3f9e825adbee5
epolyak <polyak.ev@gazprom-neft.ru>
2024-07-29 17:28:53 +0300

    Initial commit

---
+++ src/main/java/ru/polyak/telegram/bot/ext/TelegramBotExtendedApplication.java
@@ -0,0 +1,13 @@
+package ru.polyak.telegram.bot.ext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotExtendedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotExtendedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/polyak/telegram/bot/ext/TelegramBotExtendedApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.polyak.telegram.bot.ext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotExtendedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
